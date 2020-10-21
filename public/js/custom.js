97e17b5567416fb9bfc351c462bdce6d26dd218b
aaronllcam <aaronllcam@gmail.com>
2020-10-21 09:34:29 +0200

    [ADD] Some effects

---
+++ public/js/custom.js
@@ -0,0 +1,40 @@
+
+$('.select').on('click', () =>{
+    $('.content-select ul').toggle("slow");
+})
+const showPartners = () => {
+
+    const categories = document.querySelectorAll("#select-list > li");
+    const partners = document.querySelectorAll("#brand > div");
+
+    for (category of categories) {
+        //Asignamos a cada li de la lista del select el evento que escucha el click! 
+        category.addEventListener("click", function(e){
+            let category = e.target.dataset.category;
+
+            //Añadimos la clase active al elemento clicado y removemos dicha clase de los ptros elementos
+            for (categ of categories){
+                if(category == categ.dataset.category){
+                    categ.classList.add("active");
+                }else{
+                    categ.classList.remove("active");
+                }
+            }
+            //recorremos los partnerns y si pertenecen a dicha categoría los mostramos con el estilo en liena flex, al resto los escondemos
+            for (const partner of partners) {
+                if(category != partner.dataset.category){
+                    partner.style.display = "none";
+                }
+                if(category == partner.dataset.category){
+                    partner.style.display = "flex";
+                }
+                if(category == "all"){
+                    partner.style.display = "flex";
+                }
+            }
+        });
+
+    }
+}
+
+showPartners();
